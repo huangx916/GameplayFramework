fe1983a1782ee7e989cdf133addcfe98589ed6cf
huangx916 <huangx916@163.com>
2019-01-09 12:26:52 +0800

    auto compress texture and javascript obfuscator

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var tinypng_nokey = require('gulp-tinypng-nokey');
+var javascriptObfuscator = require("gulp-javascript-obfuscator");
+//var gulpcache = require("gulp-cache");
+
+
+//压缩图片
+gulp.task('default', function (cb) {
+    //gulp.src(["./build/fb-instant-games/Dragon/res/raw-assets/**/*.{png,jpg,jpeg}"])
+    //gulp.src(["./build/fb-instant-games/Dragon/res/raw-assets/[0-9][0-9]/*.{png,jpg,jpeg}"])
+    //gulp.src(["./build/fb-instant-games/Dragon/res/raw-assets/[0-9][a-f]/*.{png,jpg,jpeg}"])
+    //gulp.src(["./build/fb-instant-games/Dragon/res/raw-assets/[a-f][0-9]/*.{png,jpg,jpeg}"])
+    gulp.src(["./build/fb-instant-games/Dragon/res/raw-assets/[a-f][a-f]/*.{png,jpg,jpeg}"])
+        .pipe(tinypng_nokey())
+        .pipe(gulp.dest("./build/fb-instant-games/Dragon/res/raw-assets/"))
+        .on("end", cb);
+});
+
+// 混淆
+// gulp.task("default", function (cb) {
+//     gulp.src(["./build/fb-instant-games/Dragon/src/project.js"])// 参数含义  地址  https://github.com/javascript-obfuscator/javascript-obfuscator#javascript-obfuscator-options
+//         .pipe(javascriptObfuscator({
+//             // compact: true,//类型：boolean默认：true
+//             mangle: true,//短标识符的名称，如a，b，c
+//             stringArray: true,
+//             target: "browser",
+//         }))
+//         .pipe(gulp.dest("./build/fb-instant-games/Dragon/src/")
+//             .on("end", cb));
+// });
